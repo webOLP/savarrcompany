06ab4ea7d2b5ec89e1b5c241f0bb24e6a7fa1a1d
OLP <semtara@yandex.ru>
2022-06-17 09:41:41 +0300

    popups

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const future = document.querySelector('.future');
 const futureTitle = future.querySelector('.future__title');
-
+const popupWindow = document.querySelector('.popup-background')
+const buttonCallPopup = document.querySelectorAll('.call-popup')
 
 function textFutureTitle() {
     
@@ -11,5 +12,24 @@ function textFutureTitle() {
     }
 }
 
+function popupActive() {
+    popupWindow.classList.add('popup-background_active');
+}
 
+function popupClose(event) {
+    event.preventDefault();
+    console.log(event.currentTarget, event.target)
+    if( (event.target === event.currentTarget) || event.target === popupWindow.querySelector('.form-popup__button')) {
+        popupWindow.classList.remove('popup-background_active')
+    }
+    
+}
+
+
+
+popupWindow.addEventListener('click', popupClose);
 setInterval(textFutureTitle, 3000);
+
+for (let i = 0; i < buttonCallPopup.length; i++) {
+    buttonCallPopup[i].addEventListener('click',popupActive);
+}
